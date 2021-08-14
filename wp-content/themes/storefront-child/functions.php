9b586b535e4c710a9df71544213cffc299d1b741
Ivrro <36031438+Ivrro@users.noreply.github.com>
2021-08-14 14:35:40 +0200

    Delete wp-content/themes directory

--- wp-content/themes/storefront-child/functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
-function my_theme_enqueue_styles() {
-	wp_enqueue_style('storefront', get_template_directory_uri() . '/style.css');
-}
